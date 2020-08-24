8513bcb4d5721e64cadb9287d0022abd2d3c49dd
edynak-demo <edynakdemo@gmail.com>
2020-08-24 15:46:12 -0400

    implemented get form values

--- register.php
+++ register.php
@@ -1,3 +1,55 @@
+<?php  
+$con = mysqli_connect("localhost", "root", "", "KJconnect"); //Connection variable
+
+if(mysqli_connect_errno()) 
+{
+	echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+//Declaring variables to prevent errors
+$fname = ""; //First name
+$lname = ""; //Last name
+$em = ""; //email
+$em2 = ""; //email 2
+$password = ""; //password
+$password2 = ""; //password 2
+$date = ""; //Sign up date 
+$error_array = array(); //Holds error messages
+
+if(isset($_POST['register_button'])){
+
+	//Registration form values
+
+	//First name
+	$fname = strip_tags($_POST['reg_fname']); //Remove html tags
+	$fname = str_replace(' ', '', $fname); //remove spaces
+	$fname = ucfirst(strtolower($fname)); //Uppercase first letter
+	
+  //Last name
+	$lname = strip_tags($_POST['reg_lname']); //Remove html tags
+	$lname = str_replace(' ', '', $lname); //remove spaces
+  $lname = ucfirst(strtolower($lname)); //Uppercase first letter
+  
+  //email
+	$em = strip_tags($_POST['reg_email']); //Remove html tags
+	$em = str_replace(' ', '', $em); //remove spaces
+	$em = ucfirst(strtolower($em)); //Uppercase first letter
+
+  //email 2
+	$em2 = strip_tags($_POST['reg_email2']); //Remove html tags
+	$em2 = str_replace(' ', '', $em2); //remove spaces
+  $em2 = ucfirst(strtolower($em2)); //Uppercase first letter
+  
+  //Password
+	$password = strip_tags($_POST['reg_password']); //Remove html tags
+	$password2 = strip_tags($_POST['reg_password2']); //Remove html tags
+
+	$date = date("Y-m-d"); //Current date
+
+
+?>
+
+
 <html>
 <head>
 	<title>Welcome to KJconnect!</title>
