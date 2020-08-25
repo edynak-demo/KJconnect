73522989a4e0fa3f67f985d898a5c386ebf97500
edynak-demo <edynakdemo@gmail.com>
2020-08-25 10:16:50 -0400

    implemented name and password validation

--- register.php
+++ register.php
@@ -67,6 +67,27 @@ if(isset($_POST['register_button'])){
     echo "Emails don't match";
   }
 
+  	if(strlen($fname) > 25 || strlen($fname) < 2) {
+		echo "Your first name must be between 2 and 25 characters";
+	}
+
+	if(strlen($lname) > 25 || strlen($lname) < 2) {
+		echo "Your last name must be between 2 and 25 characters";
+	}
+
+	if($password != $password2) {
+		echo "Your passwords do not match";
+	}
+	else {
+		if(preg_match('/[^A-Za-z0-9]/', $password)) {
+			echo "Your password can only contain english characters or numbers";
+		}
+  }
+
+  if(strlen($password > 30 || strlen($password) < 5)) {
+		echo "Your password must be betwen 5 and 30 characters";
+	}
+
 }
 
 ?>
