26a6d737a6845ead6ec9bd4df8d3ff6217713dc8
edynak-demo <edynakdemo@gmail.com>
2020-08-25 11:23:27 -0400

    implemented generate random default profile pic for user feature

--- register.php
+++ register.php
@@ -109,7 +109,17 @@ if(isset($_POST['register_button'])){
 			$i++; //Add 1 to i
 			$username = $username . "_" . $i;
 			$check_username_query = mysqli_query($con, "SELECT username FROM users WHERE username='$username'");
-		}
+    }
+    
+		//Profile picture assignment
+		$rand = rand(1, 2); //Random number between 1 and 2
+
+		if($rand == 1)
+			$profile_pic = "assets/images/profile_pics/defaults/head_deep_blue.png";
+		else if($rand == 2)
+			$profile_pic = "assets/images/profile_pics/defaults/head_emerald.png";
+
+
   }
 }
 
